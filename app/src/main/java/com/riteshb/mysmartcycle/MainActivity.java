92070b6cad6cf0bfda1a07960bf7015343be9e5b
Ritesh Bhavsar <ritesh.bhavsar86@gmail.com>
2021-04-05 21:16:35 +0530

    first file

--- app/src/main/java/com/riteshb/mysmartcycle/MainActivity.java
+++ app/src/main/java/com/riteshb/mysmartcycle/MainActivity.java
@@ -42,6 +42,7 @@ import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 
+
 public class MainActivity extends YouTubeFailureRecoveryActivity implements
         OnChartValueSelectedListener/*, YouTubePlayerSeekBarListener, YouTubePlayerListener */
         , YouTubePlayer.OnInitializedListener {
